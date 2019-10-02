eb183ea96c4c8d7bd03c5bfa92898b282dab16ec
evanliu2968 <evanliu2968@gmail.com>
2019-10-03 00:37:26 +0800

    更新babel.config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,16 @@
 'use strict'
 const path = require('path')
 
+function resolve(dir) {
+  return path.join(__dirname, './', dir)
+}
+
 module.exports = {
   pages: {
     index: {
       entry: 'src/main.js',
       template: 'public/index.html',
+      // fileName: resolve('./index.html'),
       // chunks: ['chunk-vendors', 'chunk-common', 'index'],
       title: '首页'
     }
@@ -13,10 +18,19 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './dist/' : '',
   outputDir: 'dist',
   // 打包后的地址
-  indexPath: path.join(__dirname, './index.html'),
-  chainWebpack: config => config.plugins.delete('named-chunks'),
+  indexPath: resolve('./index.html'),
+  chainWebpack: config => {
+    // 修复HMR
+    // config.resolve.symlinks(true)
+    // 多页应用自定义name chunks
+    config.plugins.delete('named-chunks')
+    // 添加别名
+    config.resolve.alias
+    .set('@', resolve('src'))
+  },
   transpileDependencies: [
     // 'resize-detector',
+    'web-inject',
     'element-ui'
   ],
   devServer: {
